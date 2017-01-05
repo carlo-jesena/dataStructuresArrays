beabf666b26a9811767b32f57fab77855410605a
numbergames <bfletch@gmail.com>
2017-01-05 14:15:46 -0500

    working in browser console

--- memory.js
+++ memory.js
@@ -1,53 +1,57 @@
 // The word's worst allocator
-var memory = new Float64Array(1024);
-var head = 0;
+var memory = (function () {
+  var memory = new Float64Array(1024);
+  var head = 0;
 
-var allocate = function(size) {
+  var allocate = function(size) {
     if (head + size > memory.length) {
-        return null;
+      return null;
     }
     var start = head;
     head += size;
     return start;
-};
+  };
 
-var free = function(ptr) {
-};
+  var free = function(ptr) {
+  };
 
-var copy = function(to, from, size) {
+  var copy = function(to, from, size) {
     if (from === to) {
-        return;
+      return;
     }
     else if (from > to) {
-        // Iterate forwards
-        for (var i=0; i<size; i++) {
-            set(to + i, get(from + i));
-        }
+      // Iterate forwards
+      for (let i = 0; i < size; i++) {
+        set(to + i, get(from + i));
+      }
     }
     else {
-        // Iterate backwards
-        for (var i=size - 1; i>=0; i--) {
-            set(to + i, get(from + i));
-        }
+      // Iterate backwards
+      for (let i = size - 1; i >= 0; i--) {
+        set(to + i, get(from + i));
+      }
     }
-};
+  };
 
-var get = function(ptr) {
+  var get = function(ptr) {
     return memory[ptr];
-};
+  };
 
-var set = function(ptr, value) {
+  var set = function(ptr, value) {
     memory[ptr] = value;
-};
+  };
 
-exports.allocate = allocate;
-exports.free = free;
-exports.copy = copy;
-exports.get = get;
-exports.set = set;
+  return {
+    allocate,
+    free,
+    copy,
+    get,
+    set
+  };
+}());
 
-
-// 1 2 3 4 5 6 7 8 9 0
-// copy(6, 2, 2);
-// =>
-// 1 2 3 4 5 6 3 4 9 0
+// exports.allocate = allocate;
+// exports.free = free;
+// exports.copy = copy;
+// exports.get = get;
+// exports.set = set;
